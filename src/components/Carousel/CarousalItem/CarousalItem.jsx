6043dfe5641ee19d47e837c35ae16966b49b1d5c
MahaAlchommari <mahaalchommari@gmail.com>
2023-11-11 14:19:01 +0300

    task

---
+++ src/components/Carousel/CarousalItem/CarousalItem.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const CarousalItem = ({ item, width }) => {
+  return (
+    <div className="carousel-item" style={{ width: width }}>
+      <div></div>
+      <img className="carousel-img" src={item.icon.default} />
+      <div className="carousel-item-text">{item.description}</div>
+    </div>
+  );
+};
+
+export default CarousalItem;
---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import CarousalItem from "./CarousalItem/CarousalItem";
+
+const Carousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const items = [
+    {
+      description:
+        "Baseball is a bat-and-ball sport played between two teams of nine players each, taking turns batting and fielding. The game occurs over the course of several plays, with each play generally beginning when a player on the fielding team, called the pitcher.",
+      icon: require("./../../assets/img/undraw_undraw_undraw_completion_w34a_-2-_1ac3_l5bk.svg"),
+    },
+    {
+      description:
+        "Walking (also known as ambulation) is one of the main gaits of terrestrial locomotion among legged animals. Walking is typically slower than running and other gaits. ",
+      icon: require("././../../assets/img/undraw_undraw_undraw_search_engines_041x_-2-_cl95_o7o8.svg"),
+    },
+    {
+      description:
+        "Weightlifting generally refers to activities in which people lift weights, often in the form of dumbbells or barbells. People lift various kinds of weights for a variety of different reasons.",
+      icon: require("././../../assets/img/undraw_undraw_undraw_users_per_minute_1e4q_t22j_-1-_0ngf.svg"),
+    },
+  ];
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = 0;
+    } else if (newIndex >= items.length) {
+      newIndex = items.length - 1;
+    }
+
+    setActiveIndex(newIndex);
+  };
+  return (
+    <div className="carousel">
+      <div
+        className="inner"
+        style={{ transform: `translate(-${activeIndex * 100}%)` }}
+      >
+        {items.map((item) => {
+          return <CarousalItem item={item} width={"100%"} />;
+        })}
+      </div>
+
+      <div className="carousel-buttons">
+        <button
+          className="button-arrow"
+          onClick={() => {
+            updateIndex(activeIndex - 1);
+          }}
+        >
+          <span class="material-symbols-outlined">arrow_back_ios</span>
+        </button>
+        <div className="indicators">
+          {items.map((item, index) => {
+            return (
+              <button
+                className="indicator-buttons"
+                onClick={() => {
+                  updateIndex(index);
+                }}
+              >
+                <span
+                  className={`material-symbols-outlined ${
+                    index === activeIndex
+                      ? "indicator-symbol-active"
+                      : "indicator-symbol"
+                  }`}
+                >
+                  radio_button_checked
+                </span>
+              </button>
+            );
+          })}
+        </div>
+        <button
+          className="button-arrow"
+          onClick={() => {
+            updateIndex(activeIndex + 1);
+          }}
+        >
+          <span class="material-symbols-outlined">arrow_forward_ios</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
